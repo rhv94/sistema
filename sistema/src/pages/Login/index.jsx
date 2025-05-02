cfba30dac06657be34e7c639be21275c5e684f23
rhv94 <rebeca_hv@hotmail.com>
2025-05-02 11:53:10 -0300

    fim

--- sistema/src/pages/Login/index.jsx
+++ sistema/src/pages/Login/index.jsx
@@ -1,5 +1,23 @@
-export default function LoginPage(){
-    return (
-        <p>Aqui é a página de Login</p>
-    )
+import { Link, useNavigate } from "react-router-dom";
+import svg from "/public/tablet-login-animate.svg";
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-screen flex">
+      <div className="w-[40%] h-full flex items-center justify-center">
+        <Link to="/cadastro">Cadastrar</Link>
+        <button onClick={() => navigate("/cadastro")}>Cadastrar</button>
+        <button onClick={() => (window.location.href = "/cadastro")}>
+          Cadastrar
+        </button>
+      </div>
+      <div className="w-[60%] h-full bg-[#6366EE] flex items-center justify-center rounded-l-lg flex-col">
+        <img width={600} src={svg} alt="imagem-animada" />
+        <p className="text-white font-bold w-[300px] text-[20px] text-center">
+          A melhor experiência de login que você já teve na sua vida!
+        </p>
+      </div>
+    </div>
+  );
 }
